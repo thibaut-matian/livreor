7c5d2723a6d1f693ddd92449f6d9df0623ddd71b
Theubald-bee <thibaut.matian@gmail.com>
2025-09-26 10:25:00 +0200

    configu de connexion bdd

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// Configuration bdd
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'livreor');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+// session pour demarrer 
+session_start();
+?>
